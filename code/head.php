95e669c1ddbb8e0561a68e551a197d4df9ae413d
cunius <92263645+cunius@users.noreply.github.com>
2023-12-30 02:35:39 +0900

    Create head.php

---
+++ code/head.php
@@ -0,0 +1,50 @@
+<?php
+session_start();
+
+function isLoggedIn() {
+    return isset($_SESSION['login_user']);
+}
+
+function getUserRole() {
+    // Check if 'isAdmin' is set and equals to 1, then return 'Admin', else 'User'
+    return (isset($_SESSION['isAdmin']) && $_SESSION['isAdmin'] == 1) ? 'Admin' : 'User';
+}
+
+$current_page = basename($_SERVER['PHP_SELF']);
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>💜 왕잠탱이는 아이유를 조아행 💜</title>
+    <!-- Add other head elements like CSS files here -->
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div id="warp">
+        <header>
+            <nav>
+                <ul>
+                    <li><a href="index.php">💜 Home</a></li>
+                    <?php if (isLoggedIn()): ?>
+                        <span>환영해 우리 <?php echo htmlspecialchars($_SESSION['login_user']); ?> (<?php echo getUserRole(); ?>)💜</span>
+                        <a href="logout.php">로그아웃</a>
+                    <?php else: ?>
+                        <?php if ($current_page == "login.php"): ?>
+                            <li><a href="signup.php">💜 7기 가입</a></li>
+                        <?php else: ?>
+                            <li><a href="login.php">💜 유애나</a></li>
+                        <?php endif; ?>
+                    <?php endif; ?>
+                </ul> 
+            </nav>
+        </header>
+    </div>
+<br><br><br>
+
+<footer>
+    <br>
+    <p>© 2023 💜 왕잠탱이는 아이유를 조아행 💜 - All rights reserved.</p>
+</footer>
+</body>
+</html>
