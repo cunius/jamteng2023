297314d1abccda7df64e86448bc70987c75f5f04
cunius <92263645+cunius@users.noreply.github.com>
2023-12-30 02:12:00 -0500

    Update index.php

--- code/index.php
+++ code/index.php
@@ -1,55 +1,22 @@
 <?php
 include 'head.php';
 include 'config.php'; // Database connection
-
-$searchTerm = isset($_GET['search']) ? $conn->real_escape_string($_GET['search']) : '';
-
-$sql = "SELECT id, title, content, created_at FROM posts";
-if (!empty($searchTerm)) {
-    $sql .= " WHERE title LIKE '%$searchTerm%' OR content LIKE '%$searchTerm%'";
-}
-$sql .= " ORDER BY created_at DESC";
-$result = $conn->query($sql);
 ?>
+<h1>하루 끝에서 하는 말 내가 널 사랑해</h1>
+    <img src="iu.jpg" width="500">
+    <h3>많이 좋아서 그래 뒤돌아서 느려진 발걸음을 봐</h3>
+    <h3>널 기다리는 맘인 거잖아 날 지켜본 너라면 알잖아</h3>
+    <h3>내가 몰라서 그래 네 마음이 들리게 내게 말해줘</h3>
+    <h3>손 내밀면 될 것 같은데 망설이는 이유가 나와 같다면</h3>
+    <h3>이제 다가와 줘</h3>
 
-<!DOCTYPE html>
-<html>
-<head>
-    <title>BBS Index</title>
-    <!-- Your CSS and other head elements here -->
-</head>
-<body>
-
-<form method="GET" action="index.php">
-    <input type="text" name="search" placeholder="아이유를 찾아줘 💜" value="<?php echo htmlspecialchars($searchTerm); ?>">
-    <input type="submit" value="Go! 😙">
-</form>
-
-<?php
-// Display the current search term
-if (!empty($searchTerm)) {
-    echo "<p>너 <strong>".$searchTerm."</strong> 찾니?</p>";
-}
-
-echo "<a href='write.php'>편지 쓰기 💜</a>";
-
-if ($result->num_rows > 0) {
-    echo "<table><tr><th>Title</th><th>Actions</th></tr>";
-    while($row = $result->fetch_assoc()) {
-        echo "<tr>
-                <td><a href='read.php?id=".$row["id"]."'>".htmlspecialchars($row["title"])."</a></td>
-                <td>
-                    <a href='edit.php?id=".$row["id"]."'>Edit</a>
-                    <a href='delete.php?id=".$row["id"]."'>Delete</a>
-                </td>
-              </tr>";
-    }
-    echo "</table>";
-} else {
-    echo "아무도 편지 안 써줘떠... 🥲";
-}
-$conn->close();
-?>
-
+    <audio autoplay controls>
+        <source src="TheEndoftheDay.mp3" type="audio/mp3">
+    </audio><br><br>
+        <a href="ad.php" class="btn btn-outline-info float-right"><br>💜 유애나 7기 모집 공고! 💜</a> <br>
+        <a href="signup.php" class="btn btn-outline-info float-right"><br>💜 유애나 가입 신청서 요기! 💜</a><br>
+        <a href="write.php" class="btn btn-outline-info float-right"><br>💜 편지 쓰기 💜</a>
+    </div>
+<footer><?php include ("footer.php") ?>
 </body>
 </html>
