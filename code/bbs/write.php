7cbd0f6462019da7635f31126698cbea0b85b967
cunius <92263645+cunius@users.noreply.github.com>
2023-12-30 23:42:52 +0900

    Update write.php

--- code/bbs/write.php
+++ code/bbs/write.php
@@ -1,17 +1,26 @@
 <?php
+session_start();
 include 'head.php';
+
+//Redirect if not logged in
+if (!isset($_SESSION['login_user'])) {
+    header("Location: login.php");
+    exit();
+}
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     include 'config.php';
 
     $title = $conn->real_escape_string($_POST['title']);
     $content = $conn->real_escape_string($_POST['content']);
+    $userId = $_SESSION['userId']; //Retrieve the userId from the session
 
-    $sql = "INSERT INTO bbs (title, content) VALUES ('$title', '$content')";
+    $sql = "INSERT INTO bbs (title, content, userId) VALUES ('$title', '$content', '$userId')";
 
     if ($conn->query($sql) === TRUE) {
         header("Location: list.php");
     } else {
-        echo "Error: " . $conn->error;
+        echo "<h2>너 뭐야!? 😒</h2> " . $conn->error;
     }
     $conn->close();
 }
@@ -20,15 +29,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 <h1>💜 익명의 팬 래터 💜</h1>
 <a href="list.php">💜 편지함으로 슝~ 이동!</a><br><br>
-
-
 <img src="iu4.jpg" width="500"><br>
+
 <form method="post" action="">
     제목<br><input type="text" name="title" id="title" value="<?php echo $title ?? ''; ?>"><br><br>
     편지<br>
-    <textarea name="content" id="content"><?php echo $content ?? ''; ?></textarea><br><br>
+    <textarea name="content" rows="15" cols="63" id="content"><?php echo $content ?? ''; ?></textarea><br><br>
     <input type="submit" value="전송 💜">
 </form>
+
 <div id="preview"></div>
 
 <script>
