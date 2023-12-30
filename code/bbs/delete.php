60f9eb3dcb06ca10db0d6a835ac27477589a754b
cunius <92263645+cunius@users.noreply.github.com>
2023-12-30 09:04:12 -0500

    Update delete.php
    csrf

--- code/bbs/delete.php
+++ code/bbs/delete.php
@@ -2,17 +2,31 @@
 include 'head.php';
 include 'config.php';
 
-$id = $_GET['id']; // Get the bbs ID from the URL
-
-$sql = "DELETE FROM bbs WHERE id = $id";
-
-if ($conn->query($sql) === TRUE) {
-    echo "Post deleted successfully";
-    // Redirect to post list or confirmation page
-    header("Location: index.php");
-} else {
-    echo "Error deleting post: " . $conn->error;
+if (!isset($_SESSION['login_user'])) {
+    header("Location: login.php");
+    exit();
 }
 
-$conn->close();
+$id = $_GET['id']; // Get the bbs ID from the URL
+
+//Fetch the post creator's userId
+$sql = "SELECT userId FROM bbs WHERE id = '$id'";
+$result = $conn->query($sql);
+$row = $result->fetch_assoc();
+
+//Check prm
+if ($_SESSION['userId'] != $row['userId'] && $_SESSION['isAdmin'] != 1) {
+    echo "Beep! Beep! Dangerous!!";
+    exit();
+}
+
+//Delete the post
+$delete_sql = "DELETE FROM bbs WHERE id = '$id'";
+if ($conn->query($delete_sql) === TRUE) {
+    echo "Deleted";
+    header("Location: list.php");
+} else {
+    echo "Error!" .$conn->error;
+}
+
 ?>
