ef02eae3011cbd44c50cace34548c3f97bf1e1a8
cunius <92263645+cunius@users.noreply.github.com>
2024-01-03 05:46:27 -0500

    Update write.php
    Add fileupload func

--- code/bbs/write.php
+++ code/bbs/write.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 include 'head.php';
+include 'upload.php';
 
 //Redirect if not logged in
 if (!isset($_SESSION['login_user'])) {
@@ -14,27 +15,35 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $title = $conn->real_escape_string($_POST['title']);
     $content = $conn->real_escape_string($_POST['content']);
     $userId = $_SESSION['userId']; //Retrieve the userId from the session
+    
+    $uploadStatus = handleFileUpload();
+    if (!$uploadStatus["error"]) {
+        $filePath = conn->real_escape_string($uploadStatus["filePath"];
+        
+        $sql = "INSERT INTO bbs (title, content, userId) VALUES ('$title', '$content', '$userId', '$filePath')";
 
-    $sql = "INSERT INTO bbs (title, content, userId) VALUES ('$title', '$content', '$userId')";
-
-    if ($conn->query($sql) === TRUE) {
-        header("Location: list.php");
+        if ($conn->query($sql) === TRUE) {
+            header("Location: list.php");
+            exit();
+        } else {
+            echo "<h2>너 뭐야!? 😒</h2> " . $conn->error;
+        }
     } else {
-        echo "<h2>너 뭐야!? 😒</h2> " . $conn->error;
+        echo $uploadStatus["errorMessage"];
     }
     $conn->close();
 }
-
 ?>
 
 <h1>💜 익명의 팬 래터 💜</h1>
 <a href="list.php">💜 편지함으로 슝~ 이동!</a><br><br>
 <img src="iu4.jpg" width="500"><br>
 
-<form method="post" action="">
+<form method="post" action="" enctype="multipart/form-data">
     제목<br><input type="text" name="title" id="title" value="<?php echo $title ?? ''; ?>"><br><br>
     편지<br>
     <textarea name="content" rows="15" cols="63" id="content"><?php echo $content ?? ''; ?></textarea><br><br>
+    <input type="file" name="fileUpload"><br>
     <input type="submit" value="전송 💜">
 </form>
 
