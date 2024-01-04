fccff56dd8594a62c207271feafdaa2252066c6a
cunius <92263645+cunius@users.noreply.github.com>
2024-01-04 17:27:35 +0800

    Update getimg.php
    add fileupload

--- code/getimg.php
+++ code/getimg.php
@@ -1,6 +1,11 @@
 <?php
 if(isset($_GET['url'])) {
     $url = $_GET['url'];
+
+    if (filter_var($url, FILTER_VALIDATE_URL) === FALSE) {
+        die('Invalid URL');
+    }
+    
     // This is where the SSRF vulnerability is. The script is using a user-provided URL without any validation.
     $content = file_get_contents($url);
     header('Content-Type: image/png');
