6b2b743e468690b1b9c6e56bc0c94829af6c5919
cunius <92263645+cunius@users.noreply.github.com>
2024-01-04 17:22:29 +0800

    Update read.php

--- code/bbs/read.php
+++ code/bbs/read.php
@@ -6,15 +6,20 @@ $id = $_GET['id'];
 // $id = isset($_GET['id']) ? $conn->real_escape_string($_GET['id']) : 0;
 
 
-$sql = "SELECT title, content, created_at FROM bbs WHERE id = $id";
+$sql = "SELECT id, title, content, created_at, filePath FROM bbs WHERE id = $id";
 // $sql = "SELECT id, title, content FROM bbs WHERE id = $id";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     $row = $result->fetch_assoc();
-    echo "<h1>".$row["title"]."</h1>";
+    echo "<h2>".$row["id"].". ".$row["title"]."</h2>";
     echo "<p>".$row["content"]."</p>";
     echo "<p>보낸 날짜는 ".$row["created_at"]." 💜</p>";
+
+    if (!empty($row['filePath'])) {
+         $imageUrl = 'getimage.php?url=' . urlencode($row['filePath']);
+        echo '<img src="' . htmlspecialchars($imageUrl) . '" alt="Uploaded Image" style="max-width:80%;height:auto;">';
+    }
 } else {
     echo "<h2>아무도 편지 안 써줘떠... 🥲</h2>";
 }
